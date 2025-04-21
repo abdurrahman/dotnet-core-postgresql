df3a4ea19eea1271d9dbedc9663e744c3cde85ac
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2025-04-22 00:34:45 +0300

    remove obsoleted SetCompatibilityVersion() extension

--- src/Postgresql.Mvc/Startup.cs
+++ src/Postgresql.Mvc/Startup.cs
@@ -23,8 +23,7 @@ namespace Postgresql.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false)
-                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             
             services.Configure<CookiePolicyOptions>(options =>
             {
